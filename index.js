c7caee634601ba983b62a2534a43fe96f358c729
Danny <mr.danny951008@gmail.com>
2020-02-19 17:07:03 +0900

    Promises implementation practice

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
+    });
+  });
+};
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject("Could not write to the file");
+      resolve("success");
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed:${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body.message);
+
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("Random dog img saved to the file");
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
